7194958010009b15e951b3ad950f899e84442ee1
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-09-29 20:48:51 -0300

    Criar método para executar o menu

--- src/Sistema.java
+++ src/Sistema.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Sistema {
 
     private static Scanner capturarDados(){
-        System.out.println("Com quais dessas alternativas eu posso te ajudar?");
+        System.out.println("Com quais dessas opções eu posso te ajudar?");
         return new Scanner(System.in);
     }
 
-    public Tratamentos definirTratamento(){
+    public static Tratamentos definirTratamento(){
         Tratamentos tratamentoDefinido = new Tratamentos();
 
         int escolhaMenu = capturarDados().nextInt();
@@ -27,6 +27,7 @@ public class Sistema {
         else if (escolhaMenu == 5){
             tratamentoDefinido.tratarInseguranca();
         }
+
         return tratamentoDefinido;
     }
 
@@ -38,22 +39,25 @@ public class Sistema {
         System.out.println("Saiba que tem gente como eu que adoraria te dar uma mãozinha!");
     }
 
-    public static void executarMenu(){
-        Scanner leitor = new Scanner(System.in);
-        Usuario usuario = new Usuario();
-
+    public static void exibirOpcoesMenu(){
         System.out.println("\nNo que você acha que eu poderia te ajudar?");
         System.out.println("1 - Problemas para lidar com o luto");
         System.out.println("2 - Quero ajuda com minha ansiedade");
         System.out.println("3 - Sou tímido e queria dar um jeito nisso");
         System.out.println("4 - Não tenho muito amor próprio, as vezes acho que eu seria melhor se eu fosse um pouco diferente");
         System.out.println("5 - As vezes me sinto inseguro, mas quero quebrar essa insegurança na porrada");
-        System.out.println("6 - Voltar para o menu anterior");
-        int escolha = leitor.nextInt();
-
-        else if (escolha == 6){
-
         }
 
+    public static void executar(){
+        apresentacao();
+
+        exibirOpcoesMenu();
+        System.out.println(definirTratamento());
+
+
     }
+
+
+
+
 }

8f0cc758b1466def32f397b654d247989aa47b42
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-09-29 19:27:11 -0300

    Melhorar a exibição dos metodos tratarAlgo

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Usuario teste = new Usuario();
-        Sintoma sintomaTeste = new Sintoma("Depressao");
+        Sintoma sintomaTeste = new Sintoma("Ansiedade");
         Tratamentos tratamento = new Tratamentos("Fazer academia");
 
 
-        teste.adicionarSintoma(sintomaTeste);
-        System.out.println(teste.getSintoma().getNome());
-        if (sintomaTeste.getNome().equalsIgnoreCase("Depressao")){
-            tratamento.tratarDepressao();
-            System.out.println("O tratamento para depressão é" + tratamento.getComoTratar());
-        }
+       tratamento.tratarLuto();
+        System.out.println(tratamento.getComoTratar());
     }
-}
+    }
+

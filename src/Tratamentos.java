5af9a8de311c0756f20431540137430824526659
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-09-29 18:45:31 -0300

    Atualizar método TratarTimidez com um tratamento real

--- src/Tratamentos.java
+++ src/Tratamentos.java
@@ -45,9 +45,24 @@ public class Tratamentos {
 
     public void tratarTimidez() {
         StringBuilder concatenador = new StringBuilder();
-        concatenador.append("Para te ajudar com Timidez, faça isso");
-        concatenador.append("\nTambém ajuda muito fazer aquilo ali");
-        concatenador.append("\nE claro, nunca se esqueça de sempre fazer tal coisa");
+        concatenador.append("\nTem algumas coisas que você pode fazer para lidar com a timidez\n");
+        concatenador.append("\nAutoconhecimento");
+        concatenador.append("\nO primeiro passo é se conhecer e verificar até que ponto a timidez está impedindo de conquistar seus objetivos");
+        concatenador.append("\nPrograme seu cérebro");
+        concatenador.append("\nDiariamente diga a você mesmo que és capaz de enfrentar qualquer tipo de situação e siga firme.");
+        concatenador.append("\nFaça exercícios para praticar a fala em público");
+        concatenador.append("\nExercite enfrente ao espelho ou faça gravações de sua fala, analise o tom de sua voz e pratique a exposição de suas ideias.");
+        concatenador.append("\nVença o medo da rejeição");
+        concatenador.append("\nPense em você, em seu ponto de vista e não tema os julgamentos das outras pessoas. Dialogue, seja sorridente e exponha sua opinião sem culpa, nem sempre teremos a concordância todo mundo, mas lembre de que é apenas questão de opinião");
+        concatenador.append("\nSe desafie");
+        concatenador.append("\nSe proponha a realizar coisas que você mais tem temor. Se for falar em público, se force lentamente a falar mais, se o seu maior problema é o convívio social, tente se aproximar aos poucos das pessoas.");
+        concatenador.append("\nNão se afaste das pessoas");
+        concatenador.append("\nProcure sempre estar em contato com seus amigos, não rejeite convites para conversar e procure as pessoas que você deixou de ter contato.");
+        concatenador.append("\nProcure ajuda terapêutica");
+        concatenador.append("\nA ajuda é sempre bem vinda, não tenha receio de consultar um especialista ou um psicólogo para te ajudar a diminuir sua timidez");
+        concatenador.append("\nAcredite em você e veja a diferença em sua vida");
+        concatenador.append("\nAcredite em você e siga enfrente, você tem plenas condições de seguir em frente e vencer todo e qualquer obstáculo em sua vida.");
+        concatenador.append("\nSe Você quer Você Pode. Certamente pode!");
         comoTratar = concatenador.toString();
     }
 

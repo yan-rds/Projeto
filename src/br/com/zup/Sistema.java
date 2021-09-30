fd2d1c4ba2499ed94a3667a0b4953903b702a304
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-09-30 11:39:55 -0300

    Criar menu

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -3,6 +3,19 @@ package br.com.zup;
 import java.util.Scanner;
 
 public class Sistema {
+    private static Scanner capturarDados(String mensagem) {
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+    }
+
+
+    public static void menu() {
+        System.out.println("---- Bem vindo ao bem estar FÍSICO -----");
+        System.out.println("Digite 1 - Para descobrir o seu IMC (INDICE DE MASSA CORPORAL). ");
+        System.out.println("Digite 2 - Para  objetivo corporal . ");
+        System.out.println("Digite 3 - Para obter informações  ergonomia no home office  . ");
+        System.out.println("Digite 4 - Para sair");
+    }
 }
 
 

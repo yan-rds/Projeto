943955ed4abda5fcfb8bb11efc9f3d8ad845d2e2
Karen Almeida <karenalmeida61@hotmail.com>
2021-09-30 14:35:37 -0300

    Adicionandoindicação de  paginas web nos menus

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -100,16 +100,11 @@ public class Sistema {
 
             }
             if (pessoa1.listasSintoma.size() == 4) {
-                System.out.println(" Procure atendimento médico imediatamente. " +
-                        "\n Sempre ligue antes de ir ao médico ou posto de saúde, clínicas ou hospitais.Pessoas saudáveis " +
-                        "\n que apresentarem os sintomas leves devem acompanhar a situação em casa.Em média, os sintomas" +
-                        "\n  aparecem cinco ou seis dias após a infeção pelo vírus. No entanto, eles também podem levar até 14 dias para se manifestarem.");
+                medicoUrgencia();
+
                 menuSintoma = false;
             } else {
-                System.out.println("\n A maioria das pessoas que adoece em decorrência da COVID-19 " +
-                        "\n apresenta sintomas leves a moderados e se recupera sem tratamento especial. " +
-                        "\n No entanto, algumas desenvolvem um quadro grave e precisam de atendimento médico." +
-                        "\n Fique atento e caso os sintomas se agravem, procure um médico imediatamente");
+                medicoSemUrgencia();
                 menuSintoma = false;
             }
 
@@ -117,24 +112,40 @@ public class Sistema {
 
     }
 
+    public static void medicoUrgencia(){   System.out.println(" Procure atendimento médico imediatamente. " +
+            "\n Sempre ligue antes de ir ao médico ou posto de saúde, clínicas ou hospitais. Pessoas saudáveis " +
+            "\n que apresentarem os sintomas leves devem acompanhar a situação em casa.Em média, os sintomas" +
+            "\n aparecem cinco ou seis dias após a infeção pelo vírus. No entanto, eles também podem levar até" +
+            "\n 14 dias para se manifestarem." +
+            "\n Descubra quais são os pontos exclusivos para atendimento Covid:" +
+            "\n https://www.saopaulo.sp.leg.br/coronavirus/o-que-fazer-ao-apresentar-sintomas/\n");}
+
+    public static void medicoSemUrgencia(){System.out.println("\n A maioria das pessoas que adoece em decorrência da COVID-19 " +
+            "\n apresenta sintomas leves a moderados e se recupera sem tratamento especial. " +
+            "\n No entanto, algumas desenvolvem um quadro grave e precisam de atendimento médico." +
+            "\n Fique atento e caso os sintomas se agravem, procure um médico imediatamente " +
+            "\n Descubra qual posto de vacinação mais próximo de sua casa:"+
+            "\n https://portal.vacinaja.sp.gov.br/vaccine-centers/\n");}
+
     public static void informacoesCovid() {
         System.out.println("\n O vírus pode se espalhar pela boca ou pelo nariz de uma pessoa infectada, " +
-                "\nem pequenas partículas líquidas expelidas quando elas tossem, espirram, falam, cantam ou " +
+                "\n em pequenas partículas líquidas expelidas quando elas tossem, espirram, falam, cantam ou " +
                 "\n respiram. O tamanho dessas partículas vai de gotas respiratórias maiores até aerosois " +
                 "\n menores.\n" +
                 "\n A infecção pode ocorrer caso você inale o vírus quando estiver perto de alguém " +
                 "\n que tenha COVID-19 ou se você tocar em uma superfície contaminada e, em seguida, " +
                 "\n passar as mãos nos olhos, no nariz ou na boca. O vírus se espalha com mais facilidade em " +
                 "\n locais fechados e em multidões." +
-                "" +
-                "\n Previna-se!\n");
+                "\n Previna-se!\n" +
+                "\n Informaçoes sobre a pandemia no Estado de São Paulo:"+
+                "\n https://www.saopaulo.sp.gov.br/coronavirus/");
     }
 
     public static void orientacaoCovid() {
         System.out.println("Etiqueta respiratória\n" +
-                "Uma das formas mais importantes de prevenir a disseminação do SARS-CoV-2 é a etiqueta respiratória, a qual consiste num conjunto de medidas" +
-                "que devem ser adotadas para evitar e/ ou reduzir a disseminação de pequenas gotículas oriundas do aparelho " +
-                "respiratório, buscando evitar possível contaminação de outras pessoas que estão em um mesmo ambiente." +
+                "Uma das formas mais importantes de prevenir a disseminação do SARS-CoV-2 é a etiqueta respiratória, a qual consiste num conjunto de medidas\n " +
+                "que devem ser adotadas para evitar e/ ou reduzir a disseminação de pequenas gotículas oriundas do aparelho\n" +
+                "respiratório, buscando evitar possível contaminação de outras pessoas que estão em um mesmo ambiente.\n" +
                 "\n" +
                 "A etiqueta respiratória consiste nas seguintes ações:\n" +
                 "* Cobrir nariz e boca com lenço de papel ou com o antebraço, e nunca com as mãos ao tossir ou espirrar. Descartar adequadamente o lenço utilizado.\n" +
@@ -142,7 +153,9 @@ public class Sistema {
                 "* Manter uma distância mínima de cerca de 1 metro de qualquer pessoa tossindo ou espirrando.\n" +
                 "* Evitar abraços, beijos e apertos de mãos. Adote um comportamento amigável sem contato físico.\n" +
                 "* Higienizar com frequência os brinquedos das crianças e aparelho celular. Não compartilhar objetos de uso pessoal, como talheres, toalhas.\n" +
-                "* Evitar aglomerações, principalmente em espaços fechados e manter os ambientes limpos e bem ventilados.\n");
+                "* Evitar aglomerações, principalmente em espaços fechados e manter os ambientes limpos e bem ventilados.\n" +
+                "\n" +
+                "Para mais dicas sobre prevenção acesse o site: https://coronavirus.rs.gov.br/dicas-de-prevencao\n");
     }
 
     public static void encerrarDica() {
@@ -153,7 +166,8 @@ public class Sistema {
         Sintoma sintoma1 = new Sintoma();
         return sintoma1;
     }
-    public static void executarSistema(){
+
+    public static void executarSistema() {
         menuInicial();
         menuDica();
 
